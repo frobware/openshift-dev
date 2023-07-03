f2c726a9c6847ac768f5594b814d82e49cc276a3
Andrew McDermott <aim@frobware.com>
2023-07-03 11:15:52 +0100

    azure-upi-install-scripts/approve-csr: check CLI args

--- azure-upi-install-scripts/approve-csr.sh
+++ azure-upi-install-scripts/approve-csr.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
-KUBECONFIG="${1}"
+if [ $# -eq 0 ]; then
+    echo "No arguments provided. Please provide a path to a kubeconfig file."
+    exit 1
+fi
+
+if [ ! -f "$1" ]; then
+    echo "The provided argument is not a file or doesn't exist. Please provide a valid file."
+    exit 1
+fi
+
+kubeconfig=$1; shift
 
 echo "Approving all CSR requests until bootstrapping is complete..."
 while :
 do
-    oc --insecure-skip-tls-verify --kubeconfig="$KUBECONFIG" get csr --no-headers | grep Pending | \
+    oc --insecure-skip-tls-verify --kubeconfig="$kubeconfig" get csr --no-headers | grep Pending | \
 	awk '{print $1}' | \
-	xargs --no-run-if-empty oc --insecure-skip-tls-verify --kubeconfig="$KUBECONFIG" adm certificate approve
+	xargs --no-run-if-empty oc --insecure-skip-tls-verify --kubeconfig="$kubeconfig" adm certificate approve
     sleep 1
 done
